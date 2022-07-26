f7139e5e789e2d4d78da2301a0baade8aee39b58
Michal <ciesiel.michal@gmail.com>
2022-07-26 19:32:58 +0200

    Change in script + new files

--- convert_data.py
+++ convert_data.py
@@ -3,7 +3,6 @@ with open('data.txt', 'r') as txt_file:
 
 for idx, item in enumerate(lines):
     lines[idx] = item.split(';', 1)[1]
-    print(lines[idx])
 
 with open('/output/converted_data.txt', 'w') as txt_file:
     txt_file.writelines(lines)
